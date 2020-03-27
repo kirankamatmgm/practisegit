0ace2c3b5800f74fd1f44ab7355375a86c87333a
Kiran Kamath <[kirankamat.mgm@gmail.com]>
2020-03-27 11:49:32 +0530

    add cat functionality

--- animal.py
+++ animal.py
@@ -1,10 +1,18 @@
 import sys
 
+def cat():
+    print('meow!!!')
+
 def default():
     print('hello kiran')
 
 def main():
-    default()
+    if sys.argv[1]==' ':
+        default()
+    if sys.argv[1]=='cat':
+        cat()
+    else:
+        default()
 
 if __name__=='__main__':
     main()
